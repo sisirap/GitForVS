f8bcf9de323a28da3e570ae638c34de0b7b941da
Sisira Panchagnula <psisira@gmail.com>
2015-02-09 22:38:15 -0800

    EulerProblem1

---
+++ EulerSolutions/ProblemHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerSolutions
+{
+    class ProblemHelper
+    {
+        public int QuestionOne(int upperLimit)
+        {
+            int sum = 0;
+            for (int i = 1; i < upperLimit; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+            return sum;
+        }
+
+        public int QuestionOneLinq(int upperLimit)
+        {
+            return Enumerable.Range(1, upperLimit).Where(x => x%3 == 0 || x%5 == 0).Sum();
+        }
+    }
+}
---
+++ EulerSolutions/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerSolutions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProblemHelper helper = new ProblemHelper();
+            Console.WriteLine(" The solution to (1)Multiples of 3 and 5 is {0}", helper.QuestionOne(1000));
+            Console.WriteLine(" The solution to (1)Multiples of 3 and 5 is {0}", helper.QuestionOneLinq(1000));
+        }
+    }
+}
